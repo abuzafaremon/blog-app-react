9d31e125ddae449dd5dfeb2b3abd646c327b0931
zafaremon20 <zafar emon>
2023-01-20 23:12:10 +0600

    install and setup react-router-dom firebase tailwind-css daisy-ui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-
+      <div className="avatar online">
+        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+          <img src="https://placeimg.com/192/192/people" />
+        </div>
+      </div>
+      <button class="btn">Button</button>
     </div>
   );
 };
---
+++ src/firebase.config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKGHWdhwAXvb8mf7bagFhlDQ0jQSetkQw",
+  authDomain: "blog-app-emon.firebaseapp.com",
+  projectId: "blog-app-emon",
+  storageBucket: "blog-app-emon.appspot.com",
+  messagingSenderId: "855892713595",
+  appId: "1:855892713595:web:ebe60a8f9b1c346babc2de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
