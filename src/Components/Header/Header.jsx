42461dbce13281779c4a584950aa303afaf9af1b
zafaremon20 <zafar emon>
2023-01-21 12:33:29 +0600

    Login page ok

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 sticky top-0 z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from '../../firebase.config';
 
 export default function Login() {
+  const [signInWithGoogle, user, loading] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  if (user) {
+    navigate('/post');
+  }
+  if (loading) {
+    return <p className='text-3xl p-4'>Loading...</p>
+  }
   return (
-    <div>Login</div>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">If you want to create post in this blog website, You need to Log In with your gmail.</p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input type="text" placeholder="email" className="input input-bordered" readOnly />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input type="text" placeholder="password" className="input input-bordered" readOnly />
+              <label className="label">
+                <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn" onClick={() => signInWithGoogle()}>Continue With Google</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
